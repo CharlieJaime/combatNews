a97df265ab0a94bc5a83776a22192895196d163f
Charlie Jaime <baroonie10@gmail.com>
2016-10-19 19:54:51 -0500

    new and create actions with form and new views

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -2,4 +2,23 @@ class TopicsController < ApplicationController
   def index
 
   end
+
+  def new
+    @topic = Topic.new
+  end
+
+  def create
+    @topic = Topic.new(topic_params)
+    if @topic.save
+      redirect_to root_path
+    else
+      render "new"
+    end
+  end
+
+  private
+  def topic_params
+    params.require(:topic).permit(:title, :description)
+  end
+
 end
