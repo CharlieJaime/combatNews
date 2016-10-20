fd42af1734d97f20d0c62c68194fd1325c5a22bd
Charlie Jaime <baroonie10@gmail.com>
2016-10-20 03:01:31 -0500

    edit update and destroy actions with edit view

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -1,11 +1,10 @@
 class TopicsController < ApplicationController
-  # before_action :find_topic, only:
+  before_action :find_topic, only: [:show, :edit, :update, :destroy]
   def index
     @topics = Topic.all.order("created_at DESC")
   end
 
   def show
-    @topic = Topic.find(params[:id])
   end
 
   def new
@@ -21,12 +20,28 @@ class TopicsController < ApplicationController
     end
   end
 
+  def edit
+  end
+
+  def update
+    if @topic.update(topic_params)
+      redirect_to topic_path
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @topic.destroy
+    redirect_to root_path
+  end
+
   private
   def topic_params
     params.require(:topic).permit(:title, :description)
   end
 
-  # def find_topic
-  #   @topic = Topic.find(params[:id])
-  # end
+  def find_topic
+    @topic = Topic.find(params[:id])
+  end
 end
