de7d854cfd024fddf6ca8a1da956db4a6998c13b
Charlie Jaime <baroonie10@gmail.com>
2016-10-23 20:38:29 -0500

    reverted to older version to rebuild comments crud

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,5 @@
 class User < ApplicationRecord
   has_many :topics
-  has_many :comments
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
--- db/schema.rb
+++ db/schema.rb
@@ -10,20 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161022070636) do
+ActiveRecord::Schema.define(version: 20161022065748) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "comments", force: :cascade do |t|
-    t.text     "content"
-    t.integer  "topic_id"
-    t.integer  "user_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["topic_id"], name: "index_comments_on_topic_id", using: :btree
-    t.index ["user_id"], name: "index_comments_on_user_id", using: :btree
-  end
 
   create_table "topics", force: :cascade do |t|
     t.string   "title"
@@ -50,6 +40,4 @@ ActiveRecord::Schema.define(version: 20161022070636) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
-  add_foreign_key "comments", "topics"
-  add_foreign_key "comments", "users"
 end
